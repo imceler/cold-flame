acccfc2dc51382fc93e59b73012dc4c2320496e5
Gustavo Cañas <ghenriquez700@gmail.com>
2019-07-24 17:37:15 -0400

    Responsive design listo

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	plugins: [
 		require('postcss-preset-env')({}),
         require('css-mqpacker')({}),
+        require('postcss-custom-media')({}),
         require('autoprefixer')({}),
         // require('cssnano')({
         //     preset: 'default',
